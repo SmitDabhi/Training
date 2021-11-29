60ec5a9500b665c86ab000f1df2f8c498f8c4350
Smit Dabhi <smitdabhi.sd.17@gmail.com>
2021-11-29 22:45:35 +0530

    FAQ page added

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
 function removeFun() {
 	document.getElementById("privacy").style.display = "none";
 }
+const btnHome = document.getElementById("homebtn");
+const btnCustomer = document.getElementsByClassName("btn-customer");
+const btnService = document.getElementsByClassName("btn-service");
+
+window.addEventListener("scroll", () => {
+	let nav = document.querySelector("nav");
+	let winPos = window.scrollY > 0;
+
+	nav.classList.toggle("scroll-active", winPos);
+	btnHome.classList.toggle("b2h-btn", winPos);
+});
+
+function switchBtn() {
+	btnCustomer.classList.remove("btn-customer");
+}
