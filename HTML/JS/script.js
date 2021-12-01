952617deec06dd82cf138a4f0089965b9c799d83
Smit Dabhi <smitdabhi.sd.17@gmail.com>
2021-12-01 14:30:14 +0530

    price page added

--- HTML/JS/script.js
+++ HTML/JS/script.js
@@ -2,8 +2,6 @@ function removeFun() {
 	document.getElementById("privacy").style.display = "none";
 }
 const btnHome = document.getElementById("homebtn");
-const btnCustomer = document.getElementsByClassName("btn-customer");
-const btnService = document.getElementsByClassName("btn-service");
 
 window.addEventListener("scroll", () => {
 	let nav = document.querySelector("nav");
@@ -12,7 +10,3 @@ window.addEventListener("scroll", () => {
 	nav.classList.toggle("scroll-active", winPos);
 	btnHome.classList.toggle("b2h-btn", winPos);
 });
-
-function switchBtn() {
-	btnCustomer.classList.remove("btn-customer");
-}
