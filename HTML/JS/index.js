b6df46948c5c735fddcb92965d0cf7c379340c68
Smit Dabhi <smitdabhi.sd.17@gmail.com>
2022-01-03 18:50:29 +0530

    Adding Service History Page

--- HTML/JS/index.js
+++ HTML/JS/index.js
@@ -3,13 +3,13 @@ $(document).ready(()=>{
         if(window.scrollY>10){
 			$('.home-navbar').css({'background-color': '#525252','opacity':'0.9'});
 			$('.sp-btn').css('background-color', '#29626D');
-            $('.logonav').css({"width": "73px","height":"54px"});
+            $('.logo-nav').css({"width": "73px","height":"54px"});
 			$('#homebtn').css('display', 'block');		
             
         }else{
 			$('.home-navbar').css('background-color', 'transparent');
 			$('.sp-btn').css('background-color', 'transparent');
-            $('.logonav').css({'width': '175px','height':'130px'})
+            $('.logo-nav').css({'width': '175px','height':'130px'})
 			$('#homebtn').css('display', 'none');		
         }
     });
